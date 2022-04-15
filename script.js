08d1a505ef3aa44e7027b3d02433cf806835b19d
kesarsauce <kesarsauce@gmail.com>
2022-04-15 21:05:56 +0530

    add UI

--- script.js
+++ script.js
@@ -9,29 +9,31 @@ function computerPlay(){
 }
 
 function playRound(playerSelection, computerSelection){
-    const rules = {
-        'paper':{
-            'rock':'You Win! Paper beats Rock.',
-            'scissors': 'You Lose! Scissors beats Paper.',
-        },
-        'rock':{
-            'scissors': 'You Win! Rock beats Scissors.',
-            'paper': 'You Lose! Paper beats Rock.',
-        },
-        'scissors':{
-            'paper': 'You Win! Scissors beats Paper.',
-            'rock': 'You Lose! Rock beats Scissors.',
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "Draw";
+    }
+    switch(playerSelection.toLowerCase()){
+        case 'rock': {
+            if(computerSelection.toLowerCase()==='scissors'){
+                return 'Win';
+            }
+            break;
+        }
+        case 'paper': {
+            if(computerSelection.toLowerCase()==='rock'){
+                return 'Win';
+            }
+            break;
+        }
+        case 'scissors': {
+            if(computerSelection.toLowerCase()==='paper'){
+                return 'Win';
+            }
+            break;
         }
     }
 
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        return "It's a Draw!";
-    }
-
-    else {
-        return rules[playerSelection.toLowerCase()]
-                [computerSelection.toLowerCase()];
-    }
+    return 'Loss';
 }
 
 function game(){
@@ -64,4 +66,88 @@ function game(){
     
 }
 
-game()
+result = document.querySelector('.result');
+scoreboard = document.querySelector('.scoreboard');
+playArea = document.querySelector('.play-area');
+choices = document.querySelector('.choices');
+
+
+playerScore = document.querySelector('span#player-score');
+computerScore = document.querySelector('span#computer-score');
+
+playerChoiceImg = document.querySelector('img#player-choice');
+computerChoiceImg = document.querySelector('img#computer-choice');
+buttons = document.querySelectorAll('img.choice');
+console.log(buttons)
+
+let round = 1;
+buttons.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+    btn.classList.add('selected');
+    setTimeout(()=>btn.classList.remove('selected'), 100)
+    computerMove = computerPlay();
+    playerChoiceImg.src = `./images/${btn.id}.png`;
+    
+    computerChoiceImg.src = `./images/${computerMove}.png`;
+    let outcome = playRound(btn.id, computerMove);
+    updateScoreboard(outcome);
+    round++;
+    if(round>5){
+        console.log('hey')
+        difference = parseInt(playerScore.textContent) - parseInt(computerScore.textContent);
+        showResultScreen(difference);
+    }
+}
+    )});
+
+
+
+function updateScoreboard(outcome){
+    switch(outcome){
+        case 'Win':{
+            playerScore.textContent = parseInt(playerScore.textContent) + 1;
+            break;
+        }
+        case 'Loss':{
+            computerScore.textContent = parseInt(computerScore.textContent) + 1;
+            break;
+        }
+        case 'Draw':{
+            playerScore.textContent = parseInt(playerScore.textContent) + 0.5;
+            computerScore.textContent = parseInt(computerScore.textContent) + 0.5;
+            break;
+        }
+    }
+}
+
+function showResultScreen(difference){
+    console.log(scoreboard);
+    scoreboard.style.display = 'None';
+    playArea.style.display = 'None';
+    choices.style.display = 'None';
+    result.style.display = 'block';
+    result.style.fontSize = '45px';
+    result.style.fontFamily = 'cursive'
+    document.body.style.backgroundSize = 'contain';
+    document.body.style.backgroundRepeat = 'no-repeat';
+
+    
+    console.log(result.style)
+    if(difference>0){
+        document.body.style.backgroundImage = "url('./images/win-screen.jpg')";
+        result.textContent = 'You Win';
+        result.style.color = 'pink';
+    }
+    else if(difference<0){
+        document.body.style.backgroundImage = "url('./images/loss-screen.jpg')";
+        result.textContent = 'You Lose...';
+    }
+    else{
+        document.body.style.backgroundImage = "url('./images/draw-screen.jpg')";
+        result.textContent = "It's a Draw!";
+        result.style.color = 'white';
+    }
+}
+
+
+
